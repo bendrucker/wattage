ba46f29ed672ff674c8804741a30f1148aa38fcb
Ben Drucker <bvdrucker@gmail.com>
2016-06-27 09:21:53 -0700

    Add a file input to the player component when missing src

--- views/player.js
+++ views/player.js
@@ -1,16 +1,25 @@
 'use strict'
 
 const yo = require('yo-yo')
+const file = require('./file')
 
 module.exports = render
 
 function render (state, events) {
-  if (!state.src) return
+  if (!state.src) {
+    return yo`
+      <div>
+        ${file(null, {onFile: events.onFile})}
+      </div>
+    `
+  }
+
   return yo`
     <video
       src="${state.src}"
       controls
-      ontimeupdate=${(event) => events.onTime({time: event.target.currentTime})}>
+      ontimeupdate=${(event) => events.onTime({time: event.target.currentTime})}
+      style="width: 100%">
     </video>
   `
 }
